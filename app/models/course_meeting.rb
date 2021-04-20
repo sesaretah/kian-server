f2169c22c27691bc92525ede953f2063a805d88c
sesaretah <hs.shafiei@gmail.com>
2021-04-20 19:05:23 +0430

    1

--- app/models/course_meeting.rb
+++ app/models/course_meeting.rb
@@ -18,7 +18,7 @@ class CourseMeeting < ApplicationRecord
     form.submit
 
     modules = [28, 36, 37, 39]
-    for course_module in CourseModule.where("module_id in (?) and course_id > 58474", modules)
+    for course_module in CourseModule.where("module_id in (?)", modules)
       case course_module.module_id
       when 28
         label = ""
@@ -32,6 +32,7 @@ class CourseMeeting < ApplicationRecord
       course_id = course_module.course_id
       module_id = course_module.mid
       if !course_id.blank? && !module_id.blank? && !label.blank?
+        p course_id
         course = agent.get "https://elearn5.ut.ac.ir/mod/adobeconnect" + label.to_s + "/view.php?id=" + module_id.to_s rescue nil
         if !course.blank?
           doc = Nokogiri::HTML(course.body)
@@ -40,28 +41,26 @@ class CourseMeeting < ApplicationRecord
             table.search("tr").drop(1).each do |tr|
               tds = tr.search("td")
 
-              start_date_raw = tds[2].text.split(" ")[1]
-              p tds[2].text.split(" ")[1]
-              start_time_raw = tds[2].text.split(" ")[0]
-              start_date = start_date_raw.split("/")
-              start_time = start_time_raw.split(":")
-              start_date_g = JalaliDate.new(start_date[0].to_i, start_date[1].to_i, start_date[2].to_i, start_time[0].to_i, start_time[1].to_i)
-              p start_date_g
-              start_time_g = Time.parse("#{start_date_g.to_g} #{start_time[0].to_i}:#{start_time[1].to_i}")
-              p start_time_g
+              if !tds[2].text.blank? && tds[2].text != ""
+                start_date_raw = tds[2].text.split(" ")[1]
+                start_time_raw = tds[2].text.split(" ")[0]
+                start_date = start_date_raw.split("/")
+                start_time = start_time_raw.split(":")
+                start_date_g = JalaliDate.new(start_date[0].to_i, start_date[1].to_i, start_date[2].to_i, start_time[0].to_i, start_time[1].to_i)
+                start_time_g = Time.parse("#{start_date_g.to_g} #{start_time[0].to_i}:#{start_time[1].to_i}")
+              end
 
-              end_date_raw = tds[3].text.split(" ")[1]
-              p tds[3].text.split(" ")[1]
-              end_time_raw = tds[3].text.split(" ")[0]
-              end_date = end_date_raw.split("/")
-              end_time = end_time_raw.split(":")
-              end_date_g = JalaliDate.new(end_date[0].to_i, end_date[1].to_i, end_date[2].to_i, end_time[0].to_i, end_time[1].to_i)
-              p end_date_g
-              end_time_g = Time.parse("#{end_date_g.to_g} #{end_time[0].to_i}:#{end_time[1].to_i}")
-              p end_time_g
-
-              CourseMeeting.create(course_id: course_id, sco_id: "", start_time: start_time_g, end_time: end_time_g, duration: (end_time_g - start_time_g) / 60)
-              #course_sessions << {"start" => start_time_g, "end" => end_time_g, "duration" => duration}
+              if !tds[3].text.blank? && tds[3].text != ""
+                end_date_raw = tds[3].text.split(" ")[1]
+                end_time_raw = tds[3].text.split(" ")[0]
+                end_date = end_date_raw.split("/")
+                end_time = end_time_raw.split(":")
+                end_date_g = JalaliDate.new(end_date[0].to_i, end_date[1].to_i, end_date[2].to_i, end_time[0].to_i, end_time[1].to_i)
+                end_time_g = Time.parse("#{end_date_g.to_g} #{end_time[0].to_i}:#{end_time[1].to_i}")
+              end
+              if !start_time_g.blank? && !end_time_g.blank?
+                CourseMeeting.create(course_id: course_id, sco_id: "", start_time: start_time_g, end_time: end_time_g, duration: (end_time_g - start_time_g) / 60)
+              end
             end
           end
         end
--- app/serializers/home_serializer.rb
+++ app/serializers/home_serializer.rb
@@ -1,53 +1,51 @@
 class HomeSerializer < ActiveModel::Serializer
-    include Rails.application.routes.url_helpers
-    attributes :meeting_histogram, :bb_meeting_histogram,
-    :hour_meeting_histogram
-  
-    def meeting_histogram
-        result = []
-        meetings = Meeting.connection.exec_query("select
+  include Rails.application.routes.url_helpers
+  attributes :meeting_histogram, :bb_meeting_histogram,
+             :hour_meeting_histogram
+
+  def meeting_histogram
+    result = []
+    meetings = Meeting.connection.exec_query("select
             date_trunc('day', start_time) as Day,
             count(1)
-        from meetings
+        from course_meetings
         where start_time > '#{1.months.ago}'
         group by 1
         order by Day")
-        for meeting in meetings
-            if meeting['count'] > 20000
-                meeting['count'] = meeting['count'].to_i - (meeting['count'].to_i * 3 / 4)
-            end
-            result << meeting
-        end
-        return result
+    for meeting in meetings
+      #if meeting['count'] > 20000
+      #    meeting['count'] = meeting['count'].to_i - (meeting['count'].to_i * 3 / 4)
+      #end
+      result << meeting
     end
+    return result
+  end
 
-    def hour_meeting_histogram
-        result = []
-        meetings = Meeting.connection.exec_query("select
+  def hour_meeting_histogram
+    result = []
+    meetings = Meeting.connection.exec_query("select
             date_trunc('hour', start_time) as Day,
             count(1)
-        from meetings
+        from course_meetings
         where start_time > '#{1.months.ago}'
         group by 1
         order by Day")
-        for meeting in meetings
-            if meeting['count'] > 10000
-                meeting['count'] = meeting['count'].to_i - (meeting['count'].to_i * 3 / 4)
-            end
-            meeting['day'] = meeting['day'].to_datetime.in_time_zone("Tehran")
-            result << meeting
-        end
-        return result
+    for meeting in meetings
+      #if meeting['count'] > 10000
+      #    meeting['count'] = meeting['count'].to_i - (meeting['count'].to_i * 3 / 4)
+      #end
+      meeting["day"] = meeting["day"].to_datetime.in_time_zone("Tehran")
+      result << meeting
     end
+    return result
+  end
 
-    def bb_meeting_histogram
-        BbMeeting.connection.exec_query("select
+  def bb_meeting_histogram
+    BbMeeting.connection.exec_query("select
             date_trunc('day', start_time) as Day,
             count(1)
         from bb_meetings
         group by 1
         order by Day")
-    end
-
   end
-  
+end
