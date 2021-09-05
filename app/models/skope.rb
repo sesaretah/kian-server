fe148dc83d3b78bec8abc7b9df7ba73705dc4e0e
sesaretah <hs.shafiei@gmail.com>
2021-09-05 21:06:45 +0430

    1

--- app/models/skope.rb
+++ app/models/skope.rb
@@ -174,7 +174,8 @@ class Skope < ApplicationRecord
     # self.create(utid: "rzarghami", sections: [8])
     # self.create(utid: "ghamsari", sections: [16])
     # self.create(utid: "mrhatami", sections: [31])
-    self.create(utid: "hamidizadeh", sections: [10])
-    self.create(utid: "mmrezai", sections: [10])
+    #self.create(utid: "hamidizadeh", sections: [10])
+    #self.create(utid: "mmrezai", sections: [10])
+    self.create(utid: "hrahbar", sections: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32])
   end
 end
